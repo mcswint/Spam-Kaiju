1b13f858e56cce9883dd205c61246f00f26bace4
Troy <tgb2110@columbia.edu>
2014-11-09 18:21:52 -0500

    Corrected typos in load_brands_SA.py

--- dbsetup/load_brands_SA.py
+++ dbsetup/load_brands_SA.py
@@ -17,16 +17,16 @@ with open('brands_and_websites.csv') as datafile:
 		website = row[1]
 		cleanWeb = ""
 		count = website.count(".")
-		peices = website.split(".")
+		pieces = website.split(".")
 		if (count == 0):
 			print (website)
 			continue
 		if (count == 1):
-			#print(peices[0])
-			cleanWeb = peices[0]
+			#print(pieces[0])
+			cleanWeb = pieces[0]
 		else:
-			#print(peices[1])	
-			cleanWeb = peices[1]
+			#print(pieces[1])	
+			cleanWeb = pieces[1]
 
 		new_brand = Brand(brand_name=row[0], brand_website=website, brand_website_clean = cleanWeb)	
 		session.add(new_brand)
