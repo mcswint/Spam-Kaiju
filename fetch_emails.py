ac9afbc17e4e8bd6dc3b862e1c387528bae4a7a5
Mandy <mcswinton@gmail.com>
2014-11-16 16:43:41 -0500

    added class to parse body of the email

--- fetch_emails.py
+++ fetch_emails.py
@@ -4,56 +4,87 @@ import mailbox
 import sys
 sys.path.append('dbsetup')
 from setup_db_SA import Email, Base, Brand
-
+from parseBody import parseBody
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 def fetchEmails(username, password):
-	#should we get email via ags?
-	gmailbox = imaplib.IMAP4_SSL('imap.gmail.com')
+#should we get email via ags?
+    gmailbox = imaplib.IMAP4_SSL('imap.gmail.com')
 
-	try:
-		gmailbox.login(username, password)
-	except imaplib.IMAP4.error:
-		print ("login not successful")
-		sys.exit(0)
+    try:
+        gmailbox.login(username, password)
+    except imaplib.IMAP4.error:
+        print ("login not successful")
+        sys.exit(0)
 
-	#pull all the message numbers
-	status, box = gmailbox.select("INBOX")
-	status, msgNum = gmailbox.search(None, "ALL")
+    #pull all the message numbers
+    status, box = gmailbox.select("INBOX")
+    status, msgNum = gmailbox.search(None, "ALL")
 
-	for m in msgNum[0].split():
-		#gets individual email by number
-		status, theEmail = gmailbox.fetch(m, '(RFC822)')
-		if status != 'OK':
-			print ("Message Error", m)
-			return
-		# uses email class to parse the email
-		wholeEmail = email.message_from_bytes(theEmail[0][1])
-		#you can access each part like a dict
-		print ('From', wholeEmail['From'])
+    for m in msgNum[0].split():
+        #gets individual email by number
+        status, theEmail = gmailbox.fetch(m, '(RFC822)')
+        if status != 'OK':
+            print ("Message Error", m)
+            return
+        # uses email class to parse the email
+        wholeEmail = email.message_from_bytes(theEmail[0][1])
+        #you can access each part like a dict
+        print ('From', wholeEmail['From'])
 
 
 def fetchFromMbox(filename):
-	mbox = mailbox.mbox(filename)
-	for message in mbox:
-		print ('From', message['From'])
+    mbox = mailbox.mbox(filename)
+    addEmailsToDB(mbox)
+        #print ('From', message['From'])
 
 def matchEmailstoAddresses(email):
-	pass
-
-def addEmailsToDB(message):
     pass
 
+def addEmailsToDB(mbox):
+    engine = create_engine('sqlite:///../emails2.db')
+    Base.metadata.bind = engine
+
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    for message in mbox:
+        from_address = message['From']
+        to_address = message['To']
+        #print('To:', to_address)
+        gmail_label = message['X-Gmail-Labels']
+        #print('gmail labels:', gmail_label)
+        time_sent = message['Date']
+        #print ('time sent:', time_sent)
+        message_id = message['Message-ID']
+        #print ('message id:', message_id)
+        #email_size = ...?
+        subject_line = str(message['Subject'])
+        #subject_line = subject_line.replace("�", "")
+        #print('subject:', subject_line)
+        #encode_sub = subject_line.encode('utf-8')
+        #decode_subject = subject_line.decode('utf-8', 'ignore')
+        #print('decoded: ', decode_subject)
+        body_parser = parseBody() 
+        body_plain = body_parser.getBody(message) 
+        #print (body_plain[1:100])
+        new_email = Email(sender_address=from_address, to_address=to_address,
+                    time_sent=time_sent, message_id=message_id, subject_line=subject_line,
+                    body_plain = body_plain)    
+
+        session.add(new_email)
+    session.commit()
+
 def main(argv):
-	if (len(argv) ==1):
-		fetchFromMbox(argv[0])
-	elif (len(argv) ==2):
-		fetchEmails(argv[0], argv[1])
-	else:
-		print ("Incorrect number of args")
-		sys.exit(0)
+    if (len(argv) ==1):
+        fetchFromMbox(argv[0])
+    elif (len(argv) ==2):
+        fetchEmails(argv[0], argv[1])
+    else:
+        print ("Incorrect number of args")
+        sys.exit(0)
 
 
 
---
+++ parseBody.py
@@ -0,0 +1,84 @@
+import mailbox
+import sys
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self): 
+        super().__init__()
+        self.datastring = ""
+    def handle_data(self, data):
+        self.datastring += data
+        
+
+    def getDatastring(self):
+        return self.datastring
+
+#
+class parseBody():
+
+    def getcharsets(self, msg):
+        #get charsets contained in email
+        charsets = set({})
+        for c in msg.get_charsets():
+            if c is not None:
+                charsets.update([c])
+        return charsets
+
+    def decode(self, msg, body):
+        #for each charset in email, try to decodes
+        for charset in self.getcharsets(msg):
+            try:
+                body = body.decode(charset)
+            except UnicodeDecodeError:
+                continue    
+            except AttributeError:
+                continue
+        return body    
+
+    def convertHTML(self, msg):
+        #if msg is html type, decode first, then parse html
+        body = msg.get_payload(decode=True)
+        if (body != None):
+            body = self.decode(msg, body)
+            parser = MyHTMLParser()
+            parser.feed(body)
+            parsed = parser.getDatastring()
+            return parsed
+        else:
+            return ""
+
+    #takes in msg and returns body in text format
+    def getBody(self, msg):
+        body = None
+        #if msg is multipart, walk through each section to find the text 
+        if msg.is_multipart():    
+            for part in msg.walk():
+
+                #if the part is multipart, go through each part of the email
+                #to find the text
+                if part.is_multipart(): 
+
+                    for subpart in part.walk():
+                        if subpart.get_content_type() == 'text/plain':
+                            body = subpart.get_payload(decode=True) 
+                            return self.decode(msg, body)   
+
+                        elif subpart.get_content_type() == 'text/html':
+                            return self.convertHTML(msg)
+
+                # Part isn't multipart so get the email body
+                elif part.get_content_type() == 'text/plain':
+                    body = part.get_payload(decode=True)
+                    return self.decode(msg, body) 
+
+                elif part.get_content_type() == 'text/html':
+                    return self.convertHTML(msg)      
+
+        elif msg.get_content_type() == 'text/plain':
+            body = msg.get_payload(decode=True) 
+            return self.decode(msg,body)
+
+        elif msg.get_content_type() == 'text/html':
+            return self.convertHTML(msg)
+
+
