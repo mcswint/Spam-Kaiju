cf19b6e1b104bf7365bac3ed0f3e9c65010ab645
OB Pro <OBPro@OBs-MacBook-Pro.home>
2014-11-16 12:59:36 -0500

    Parses links but crashes half way through. Trying Beautifulsoup over regex

--- parse_image_links.py
+++ parse_image_links.py
@@ -1,8 +1,11 @@
-
-
-
+import re
 
 def parse_links(message):
     #find any link and print it out
-    print(message)
-    input("Press Enter to continue...")
+    list = []
+    for key,value in message.items():
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', value)
+        list.extend(urls)
+    if list:
+        print(list)
+    
