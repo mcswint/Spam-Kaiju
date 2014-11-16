e84bf0e0adb97b46789b323dd696d06d1c294252
OB Pro <OBPro@OBs-MacBook-Pro.home>
2014-11-16 13:41:16 -0500

    Parsing works for all strings but not nested lists

--- parse_image_links.py
+++ parse_image_links.py
@@ -1,11 +1,13 @@
 import re
+import types
 
 def parse_links(message):
     #find any link and print it out
     list = []
     for key,value in message.items():
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', value)
-        list.extend(urls)
+        if type(value) is str:
+            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', value)
+            list.extend(urls)
     if list:
         print(list)
     
